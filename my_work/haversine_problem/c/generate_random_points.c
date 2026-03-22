b983d9a2ae2e446bac379be21a8421e4fa532b40
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2026-03-22 15:19:55 +0530

    Added profiler for generating random points

--- my_work/haversine_problem/c/generate_random_points.c
+++ my_work/haversine_problem/c/generate_random_points.c
@@ -10,32 +10,48 @@
 #define NOB_IMPLEMENTATION
 #include "thirdparty/nob.h"
 #define JIM_IMPLEMENTATION
+#define JIM_SCOPES_CAPACITY (1 << 16)
 #include "thirdparty/jim.h"
+#define NOB_PROFILER_IMPLEMENTATION
+#include "thirdparty/nob_profiler.h"
+
+Profiler profiler = {0};
 
 double random_double(double min, double max) {
+    start_profile(&profiler, "random_double");
     double r = ((double) random()) / ((double) RAND_MAX);
     double range = max - min;
-    return (r * range) + min;
+    double ret = (r * range) + min;
+    end_profile(&profiler, 0);
+    return ret;
 }
 
 size_t random_size_t(size_t max) {
-    return (size_t) (random_double(0, 1) * max);
+    start_profile(&profiler, "random_size_t");
+    size_t ret = (size_t) (random_double(0, 1) * max);
+    end_profile(&profiler, 0);
+    return ret;
 }
 
 bool parse_cstr_as_long(const char *str, long *res) {
+    bool result = true;
+    start_profile(&profiler, "parse_cstr_as_long");
     char *endptr;
     errno = 0;
     long val = strtol(str, &endptr, 10);
     if (str == endptr) { // No digits found
         nob_log(ERROR, "Could not parse any integer!");
-        return false;
+        return_defer(false);
     }
     if ((val == LONG_MAX || val == LONG_MIN) && errno == ERANGE) { // Out of range
         nob_log(ERROR, "Value out of range!");
-        return false;
+        return_defer(false);
     }
     *res = val;
-    return true;
+
+defer:
+    end_profile(&profiler, 0);
+    return result;
 }
 
 typedef enum {
@@ -46,13 +62,18 @@ typedef enum {
 const char *method_as_str[] = {"uniform", "cluster"};
 
 bool parse_as_method(const char *str, Method *method) {
+    bool result = false;
+    start_profile(&profiler, "parse_as_method");
     for (size_t i = 0; i < ARRAY_LEN(method_as_str); i++) {
         if (strcmp(str, method_as_str[i]) == 0) {
             *method = (Method) i;
-            return true;
+            return_defer(true);
         }
     }
-    return false;
+
+defer:
+    end_profile(&profiler, 0);
+    return result;
 }
 
 typedef struct {
@@ -73,8 +94,9 @@ void usage(const char *program) {
 }
 
 bool generate(Arguments args) {
-    static long buffer_size = 100000;
-    bool result = false;
+    static long buffer_size = 1000000;
+    bool result = true;
+    start_profile(&profiler, "generate");
     FILE *out_file_fp = NULL;
     Jim jim = {0};
     // Jim jim = {.pp = 4};
@@ -93,33 +115,46 @@ bool generate(Arguments args) {
         jim_member_key(&jim, "pairs");
         jim_array_begin(&jim);
             switch (args.method) {
-            #define populate_points                                                                           \
-                for (long count = 0; count < args.num_pair_points; count++) {                                 \
-                    if (count > 0 && count % buffer_size == 0) {                                              \
-                        nob_log(INFO, "Writing %ld pairs into the file: %s", buffer_size, args.out_file);     \
-                        if (!fwrite(jim.sink, jim.sink_count, 1, out_file_fp)) return_defer(false);           \
-                        jim.sink_count = 0;                                                                   \
-                    }                                                                                         \
-                    custom_stmt                                                                               \
-                    double x0 = random_x;                                                                     \
-                    double y0 = random_y;                                                                     \
-                    double x1 = random_x;                                                                     \
-                    double y1 = random_y;                                                                     \
-                    double hd = ReferenceHaversine(x0, y0, x1, y1, earth_radius);                             \
-                    jim_object_begin(&jim);                                                                   \
-                        jim_member_key(&jim, "x0")                  ; jim_float(&jim, x0);                    \
-                        jim_member_key(&jim, "y0")                  ; jim_float(&jim, y0);                    \
-                        jim_member_key(&jim, "x1")                  ; jim_float(&jim, x1);                    \
-                        jim_member_key(&jim, "y1")                  ; jim_float(&jim, y1);                    \
-                        jim_member_key(&jim, "reference_haversine") ; jim_float(&jim, hd);                    \
-                    jim_object_end(&jim);                                                                     \
-                }
+            #define populate_points(label)                                                              { \
+                for (long count = 0; count < args.num_pair_points; count++) {                             \
+                    bool result = true;                                                                   \
+                    if (count > 0 && count % buffer_size == 0) {                                          \
+                        start_profile(&profiler, "Writing to disk");                                      \
+                        nob_log(INFO, "Writing %ld pairs into the file: %s", buffer_size, args.out_file); \
+                        if (!fwrite(jim.sink, jim.sink_count, 1, out_file_fp)) {                          \
+                            result = false;                                                               \
+                            goto defer_if_##label;                                                        \
+                        }                                                                                 \
+                        jim.sink_count = 0;                                                               \
+                    defer_if_##label:                                                                     \
+                        end_profile(&profiler, 0);                                                        \
+                        if (!result) return_defer(false);                                                 \
+                    }                                                                                     \
+                    start_profile(&profiler, "generate_single_point");                                    \
+                    custom_stmt                                                                           \
+                    double x0 = random_x;                                                                 \
+                    double y0 = random_y;                                                                 \
+                    double x1 = random_x;                                                                 \
+                    double y1 = random_y;                                                                 \
+                    double hd = ReferenceHaversine(x0, y0, x1, y1, earth_radius);                         \
+                    start_profile(&profiler, "generate_single_point_as_json");                            \
+                    jim_object_begin(&jim);                                                               \
+                        jim_member_key(&jim, "x0")                  ; jim_float(&jim, x0);                \
+                        jim_member_key(&jim, "y0")                  ; jim_float(&jim, y0);                \
+                        jim_member_key(&jim, "x1")                  ; jim_float(&jim, x1);                \
+                        jim_member_key(&jim, "y1")                  ; jim_float(&jim, y1);                \
+                        jim_member_key(&jim, "reference_haversine") ; jim_float(&jim, hd);                \
+                    jim_object_end(&jim);                                                                 \
+                    end_profile(&profiler, 0);                                                            \
+                    end_profile(&profiler, 0);                                                            \
+                }                                                                                         \
+            }
 
             case UNIFORM: {
                 #define custom_stmt
                 #define random_x random_double(-180, 180)
                 #define random_y random_double(-90, 90)
-                populate_points
+                populate_points(UNIFORM)
                 #undef custom_stmt
                 #undef random_x
                 #undef random_y
@@ -139,7 +174,7 @@ bool generate(Arguments args) {
                 #define custom_stmt size_t i = random_size_t(NUM_CLUSTER);
                 #define random_x random_double(x_mins[i], x_maxs[i]);
                 #define random_y random_double(y_mins[i], y_maxs[i]);
-                populate_points
+                populate_points(CLUSTER)
                 #undef custom_stmt
                 #undef random_x
                 #undef random_y
@@ -156,16 +191,23 @@ bool generate(Arguments args) {
     jim_object_end(&jim);
 
     if (jim.sink_count > 0) {
+        start_profile(&profiler, "Writing to disk (remaining)");
         if (args.num_pair_points % buffer_size > 0) {
             nob_log(INFO, "Writing %ld pairs into the file: %s", args.num_pair_points % buffer_size, args.out_file);
         }
         nob_log(INFO, "Writing rest of stuff to file: %s", args.out_file);
-        if (!fwrite(jim.sink, jim.sink_count, 1, out_file_fp)) return_defer(false);
+        if (!fwrite(jim.sink, jim.sink_count, 1, out_file_fp)) {
+            result = false;
+            goto defer_if2;
+        }
+    defer_if2:
+        end_profile(&profiler, 0);
+        if (!result) return_defer(false);
     }
 
     nob_log(INFO, "Generated file: `%s`", args.out_file);
-    result = true;
 defer:
+    end_profile(&profiler, 0);
     if (!result) {
         nob_log(ERROR, "Error in generating file: `%s`", args.out_file);
     }
@@ -180,6 +222,7 @@ int main(int argc, char **argv) {
     Arguments args = {0};
     args.program = shift(argv, argc);
     assert(args.program != NULL);
+    reset_profiler(&profiler);
     if (argc <= 0) {
         usage(args.program);
         nob_log(ERROR, "<method> not provided!");
@@ -225,6 +268,7 @@ int main(int argc, char **argv) {
     // nob_log(INFO, "  <out_file>       : %s" , args.out_file);
 
     if (!generate(args)) return_defer(1);
+    log_profiler(profiler);
 
     result = 0;
 defer:
